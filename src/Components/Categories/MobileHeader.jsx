558e12e7c5c06cb1df6bc3590c740b8240f9d53a
jvittor <102699222+jvittor@users.noreply.github.com>
2024-04-05 07:26:42 -0300

    :recycle:  refactor: add menu to hamburger icon

--- src/Components/Categories/MobileHeader.jsx
+++ src/Components/Categories/MobileHeader.jsx
@@ -1,10 +1,20 @@
-import React from "react";
-import { Box, Flex, SimpleGrid, Text, Center, Image, Icon } from "@chakra-ui/react";
-import { FaBars } from "react-icons/fa";
+import React, { useState } from "react";
+import { Box, Flex, SimpleGrid, Text, Center, Image, Icon, Button } from "@chakra-ui/react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../../assets/99designsLogo.png";
 import BreadCrumbs from "./BreadCrumbs";
 
 export default function MobileHeader() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
     return (
         <SimpleGrid
             columns={3} 
@@ -16,14 +26,12 @@ export default function MobileHeader() {
         >
             <Box h={"80px"}>
                 <Flex
-                mt={4}
-                ml={6}
+                    mt={4}
+                    ml={6}
                 >
-                    <FaBars fontSize={"22px"}/>
+                    <FaBars fontSize={"22px"} onClick={toggleMenu} />
                 </Flex>
-                <Flex
-                ml={6}
-                >
+                <Flex ml={6}>
                     <BreadCrumbs />
                 </Flex>
             </Box>
@@ -34,12 +42,28 @@ export default function MobileHeader() {
             </Box>
             <Box h={"80px"}>
                 <Text
-                textAlign={"right"}
-                mt={4}
-                mr={6}
+                    textAlign={"right"}
+                    mt={4}
+                    mr={6}
                 >
                     Log in
                 </Text>
+            </Box>
+            <Box
+                position="fixed"
+                top="0"
+                left="0"
+                bottom="0"
+                width={menuOpen ? "80%" : 0}
+                bg="white"
+                opacity={menuOpen ? 1 : 0}
+                visibility={menuOpen ? "visible" : "hidden"}
+                transition="opacity 0.5s ease, width 0.5s ease"
+            >
+                {/* conteúdo do menu */}
+                <Button onClick={closeMenu} mt={4} ml={6} colorScheme="blue">
+                    Fechar
+                </Button>
             </Box>
         </SimpleGrid>
     );
