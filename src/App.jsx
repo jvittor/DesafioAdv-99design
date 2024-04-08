6eff25c9927a1cf152d668545ad6f05e5dfb9b43
jvittor <102699222+jvittor@users.noreply.github.com>
2024-04-08 05:51:13 -0300

    :recycle: refactor: add brand identity pack route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Categories from "./Components/Categories/GridCategories";
+import  BrandIdentityPack from "./Components/BrandIdentityPack/BrandIdentityPack";
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Categories />} />
+          <Route path="/brand-identity-pack/details" element={<BrandIdentityPack />} />
           {/* <Route path="*" element={<Pagina404 />} />  alterar depois*/}
         </Routes>
       </BrowserRouter>
