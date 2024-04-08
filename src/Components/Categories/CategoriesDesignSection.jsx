b9166e1880c9b55e5bce913c6feee5cc20ca006e
jvittor <102699222+jvittor@users.noreply.github.com>
2024-04-08 06:04:35 -0300

    :sparkles: feat: Categories Design Section

---
+++ src/Components/Categories/CategoriesDesignSection.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { SimpleGrid, Box, Text, HStack } from "@chakra-ui/react";
+import CarouselDesigns from "./CarouselDesigns";
+
+export default function CategoriesDesignSection() {
+
+    return (
+        <>
+            <Box
+            >
+            <Text
+            pos={"relative"}
+            top={10}
+            fontFamily={"Nunito Sans"}
+            fontSize={"24px"}
+            fontWeight={"bold"}
+         
+            >
+                What do you need designed?
+            </Text>
+            </Box>
+            
+            
+            
+        </>
+    );
+}
